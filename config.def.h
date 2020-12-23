6e30b4a8fd0053e2981e88d98f07e3c19c22a40b
acidDrain <jmgrow@gmail.com>
2020-12-23 11:04:16 -0600

    Updates to fonts

--- config.def.h
+++ config.def.h
@@ -5,8 +5,17 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code";
-static char *font2[] = { "MesloLGS NF:hinting=true:hintstyle=hintslight:antialias=true:autohint=true:rgba=rgb" };
+static char *font = "Fira Code Nerd Font:size=10:hinting=true:hintstyle=hintfull:antialias=true:autohint=true:rgba=rgb";
+
+static char *font2[] = {
+  "MesloLGS Nerd Font:size=10:hinting=true:hintstyle=hintslight:antialias=true:autohint=true:rgba=rgb",
+  "Material Icons:style=Regular:size=10:hinting=true:hintstyle=hintslight:antialias=true:autohint=true:rgba=rgb",
+  "unifont:style=Regular:size=10:hinting=true:hintstyle=hintslight:antialias=true:autohint=true:rgba=rgb",
+  "Misc Termsynu:style=Regular:size=10:hinting=true:hintstyle=hintslight:antialias=false:autohint=true:rgba=rgb",
+  "NotoSansDisplay Nerd Font:style=Regular:size=10:hinting=true:hintstyle=hintslight:antialias=true:autohint=true:rgba=rgb",
+  "Font Awesome 5 Free:style=Regular:size=10:hinting=true:hintstyle=hintslight:antialias=true:autohint=true:rgba=rgb"
+};
+
 static int borderpx = 12;
 /* static char *font = "Fira Code:hinting=true:hintstyle=hintfull:antialias=true:autohint=true:rgba=rgb"; */
 /* static char *font2[] = { "MesloLGS NF:hinting=true:hintstyle=hintfull:antialias=true:autohint=true:rgba=rgb" }; */
